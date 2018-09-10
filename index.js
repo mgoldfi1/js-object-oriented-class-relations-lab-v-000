b7aea1d11b900cea01fc9a23d16321f61eb117e5
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-10 13:57:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ class Driver {
     return store.trips.filter(function(x) {return x.driverId === this.id}.bind(this))
   }
   passengers() {
-
+    debugger;
     return this.trips().map(function(x){return x.passenger})
   }
 
