c41d07162711453511e6c158b8b33f84201dc46d
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-10 13:59:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,57 @@
+
+let store = {drivers: [], passengers: [], trips: []}
+let driverId = 0
+let passengerId = 0
+let tripId = 0
+class Driver {
+  constructor(name) {
+    this.name = name
+    this.id = ++driverId
+    store.drivers.push(this)
+  }
+
+  trips () {
+    return store.trips.filter(function(x) {return x.driverId === this.id}.bind(this))
+  }
+  passengers() {
+
+    return this.trips().map(function(x){return x.passenger()})
+  }
+
+
+}
+
+class Passenger {
+  constructor(name) {
+    this.name = name
+    this.id = ++passengerId
+    store.passengers.push(this)
+  }
+  trips () {
+    return store.trips.filter(function(x) {return x.passengerId === this.id}.bind(this))
+  }
+  drivers() {
+
+    return this.trips().map(function(x){return x.driver()})
+  }
+
+
+}
+
+class Trip {
+
+  constructor(driver,passenger) {
+    this.id = ++tripId
+    this.driverId = driver.id
+    this.passengerId = passenger.id
+    store.trips.push(this)
+  }
+
+  passenger () {
+    return store.passengers.find(function(x) {return x.id === this.passengerId}.bind(this))
+  }
+  driver () {
+    return store.drivers.find(function(x) {return x.id === this.driverId}.bind(this))
+  }
+
+}
